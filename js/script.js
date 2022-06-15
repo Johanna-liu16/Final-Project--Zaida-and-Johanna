e302028384ac9d5b365333640a43a4deca55cef1
Johanna Liu <99354992+Johanna-liu16@users.noreply.github.com>
2022-06-15 23:29:50 +0000

    fixed html

--- js/script.js
+++ js/script.js
@@ -1,110 +1,119 @@
+// Copyright (c) 2022 Johanna Liu, Zaida Hammel All rights reserved
+//
+// Created by: Johanna Liu
+// Created on: June 2022
+// This file contains the JS functions for calculator.html
+
+/**
+ * This function does the calculation.
+ */
 const calculator = {
-  displayValue: '0',
+  displayValue: "0",
   firstOperand: null,
   waitingForSecondOperand: false,
   operator: null,
-};
+}
 
 function inputDigit(digit) {
-  const { displayValue, waitingForSecondOperand } = calculator;
+  const { displayValue, waitingForSecondOperand } = calculator
 
   if (waitingForSecondOperand === true) {
-    calculator.displayValue = digit;
-    calculator.waitingForSecondOperand = false;
+    calculator.displayValue = digit
+    calculator.waitingForSecondOperand = false
   } else {
     calculator.displayValue =
-      displayValue === '0' ? digit : displayValue + digit;
+      displayValue === "0" ? digit : displayValue + digit
   }
 }
 
 function inputDecimal(dot) {
   if (calculator.waitingForSecondOperand === true) {
-    calculator.displayValue = '0.';
-    calculator.waitingForSecondOperand = false;
-    return;
+    calculator.displayValue = "0."
+    calculator.waitingForSecondOperand = false
+    return
   }
 
   if (!calculator.displayValue.includes(dot)) {
-    calculator.displayValue += dot;
+    calculator.displayValue += dot
   }
 }
 
 function handleOperator(nextOperator) {
-  const { firstOperand, displayValue, operator } = calculator;
-  const inputValue = parseFloat(displayValue);
+  const { firstOperand, displayValue, operator } = calculator
+  const inputValue = parseFloat(displayValue)
 
   if (operator && calculator.waitingForSecondOperand) {
-    calculator.operator = nextOperator;
-    return;
+    calculator.operator = nextOperator
+    return
   }
 
   if (firstOperand == null && !isNaN(inputValue)) {
-    calculator.firstOperand = inputValue;
+    calculator.firstOperand = inputValue
   } else if (operator) {
-    const currentValue = firstOperand || 0;
-    const result = calculate(currentValue, inputValue, operator);
+    const currentValue = firstOperand || 0
+    const result = calculate(currentValue, inputValue, operator)
 
-    calculator.displayValue = String(result);
-    calculator.firstOperand = result;
+    calculator.displayValue = String(result)
+    calculator.firstOperand = result
   }
 
-  calculator.waitingForSecondOperand = true;
-  calculator.operator = nextOperator;
+  calculator.waitingForSecondOperand = true
+  calculator.operator = nextOperator
 }
 
 function calculate(firstOperand, secondOperand, operator) {
   if (operator === '+') {
-    return firstOperand + secondOperand;
+    return firstOperand + secondOperand
   } else if (operator === '-') {
-    return firstOperand - secondOperand;
+    return firstOperand - secondOperand
   } else if (operator === '*') {
-    return firstOperand * secondOperand;
+    return firstOperand * secondOperand
   } else if (operator === '/') {
-    return firstOperand / secondOperand;
+    return firstOperand / secondOperand
   }
 
-  return secondOperand;
+  return secondOperand
 }
 
 function resetCalculator() {
-  calculator.displayValue = '0';
-  calculator.firstOperand = null;
-  calculator.waitingForSecondOperand = false;
-  calculator.operator = null;
+  calculator.displayValue = '0'
+  calculator.firstOperand = null
+  calculator.waitingForSecondOperand = false
+  calculator.operator = null
 }
 
 function updateDisplay() {
-  const display = document.querySelector('.calculator-screen');
-  display.value = calculator.displayValue;
+  const display = document.querySelector('.calculator-screen')
+  display.value = calculator.displayValue
 }
 
 updateDisplay();
 
-const keys = document.querySelector('.calculator-keys');
+const keys = document.querySelector('.calculator-keys')
 keys.addEventListener('click', event => {
-  const { target } = event;
+  const { target } = event
   if (!target.matches('button')) {
-    return;
+    return
   }
 
   if (target.classList.contains('operator')) {
-    handleOperator(target.value);
-    updateDisplay();
-    return;
+    handleOperator(target.value)
+    updateDisplay()
+    return
   }
 
   if (target.classList.contains('decimal')) {
-    inputDecimal(target.value);
-    updateDisplay();
-    return;
+    inputDecimal(target.value)
+    updateDisplay()
+    return
   }
 
   if (target.classList.contains('all-clear')) {
-    resetCalculator();
-    updateDisplay();
-    return;
+    resetCalculator()
+    updateDisplay()
+    return
   }
 
-  inputDigit(target.value);
-  updateDisplay();
-});
+  inputDigit(target.value)
+  updateDisplay()
+})
